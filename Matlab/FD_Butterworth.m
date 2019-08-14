85d0b65baa7571f30d983b5403b0551f15fdf44a
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-08-14 20:18:36 +0100

    Fourier Transformation

--- Matlab/FD_Butterworth.m
+++ Matlab/FD_Butterworth.m
@@ -16,3 +16,17 @@ Im = rgb2gray(Im);
 Figure;
 subplot(2,3,1);imshow(ImOrg);title('Original Image');
 subplot(2,3,2);imshow(Im);title('Noisy Image');
+
+% Fourier Transform and centered
+af = fftshift(fft2(Im));
+
+% calling function butterlp with input as image, cutoff value and order
+%Butterworth Low pass filter
+lp = butterlp(Im, 10 , 0.25); %10,0.25
+
+% Apply Filter
+aflp = af.*lp;
+% display fourier transformed image post filter 
+disftim = fftshow(aflp);
+% plot image
+subplot(2,3,3);imshow(disftim);title('Butterworth image post applying filter');
